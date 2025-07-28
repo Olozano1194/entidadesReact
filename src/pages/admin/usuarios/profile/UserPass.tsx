19f7007efae00c7df5d401ed9e76a442866c6d73
Oscar Lozano <olozano1194@gmail.com>
2025-07-28 05:35:02 -0500

    feat: modificar el componente 'Profile', para que mostrara el email del usuario logueado.

--- src/pages/admin/usuarios/profile/UserPass.tsx
+++ src/pages/admin/usuarios/profile/UserPass.tsx
@@ -28,31 +28,31 @@ function UserPass() {
     return (
         <>
              {/* Usuario y contraseña */}
-        <div className="bg-primary p-8 rounded-xl mt-10">
+        <section className="bg-primary p-8 rounded-xl mt-10">
             <h1 className="text-xl text-white font-bold md:text-2xl">Usuario y Contraseña</h1>
             <hr className="my-8 border-gray-500" />
 
             <form action="">
                 {/* Email */}
-                <div className="flex flex-col md:flex-row md:items-center justify-between mb-8">
+                <section className="flex flex-col md:flex-row md:items-center justify-between mb-8">
                     <div className="mb-4">
                         <h5 className="text-white font-semibold text-xl">Corre Electrónico</h5>
                         <p className="text-gray-400 text-sm">{user.email}</p>
                     </div>
                         <button className="py-2 px-4 rounded-lg bg-sky-700 cursor-pointer font-bold text-white hover:bg-sky-500 hover:scale-105 transition-colors" >Cambiar Correo</button>
-                </div>
+                </section>
 
                 {/* Contraseña */}
-                <div className="flex flex-col md:flex-row md:items-center justify-between mb-8">
+                <section className="flex flex-col md:flex-row md:items-center justify-between mb-8">
                     <div className="mb-3">
                         <h5 className="text-white font-semibold text-xl">Contraseña</h5>
                         <p className="text-gray-400 text-sm">***************</p>
                     </div>
                         <button className="py-2 px-4 rounded-lg bg-sky-700 cursor-pointer font-bold text-white hover:bg-sky-500 hover:scale-105 transition-colors" >Cambiar Contraseña</button>
-                </div>
+                </section>
             </form>
 
-            <div className="grid grid-cols-1 items-center gap-y-4 md:grid-cols-8 bg-blue-400/10 p-4 rounded-lg border border-dashed border-sky-400 ">
+            <article className="grid grid-cols-1 items-center gap-y-4 md:grid-cols-8 bg-blue-400/10 p-4 rounded-lg border border-dashed border-sky-400 ">
                 <div className="flex justify-center">
                     <RiShieldCheckLine className="text-4xl text-sky-400" />
                 </div>
@@ -66,8 +66,8 @@ function UserPass() {
                     <button className="bg-sky-700 cursor-pointer py-2 px-3
                     rounded-lg text-white hover:bg-sky-500 transition-colors">Activar</button>
                 </div>
-            </div>
-        </div>
+            </article>
+        </section>
         </>
     );
 }
