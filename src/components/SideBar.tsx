1bf74f9170392b07e1af89d8e4ccae0621dfea45
Oscar Lozano <srempalador2021@gmail.com>
2025-08-30 23:56:53 -0500

    feat(SideBar): Refactor sidebar to use SidebarStudent component for better organization and maintainability

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -12,6 +12,7 @@ import type { User } from "../types/user.model";
 import type { Rol } from "../types/rol.models";
 // Components
 import SidebarAdmin from "./sidebar/admin/SidebarAdmin";
+import SidebarStudent from "./sidebar/student/SidebarStudent";
 
 export interface SubMenuState {
     menu1: boolean,
@@ -143,53 +144,10 @@ const SideBar = () => {
                                     </ul> 
                                 </ul>
                             ) : (
-                                <ul className="">
-                                    {/* Home */}
-                                    <ul className="flex flex-col gap-2">
-                                        <li>
-                                            <Link to='/admin' className="flex items-center gap-3 py-2 px-4 rounded-lg hover:bg-teal-400 text-white font-semibold transition-colors"><RiHome8Line className="text-secondary-500" /> Inicio
-                                            </Link>
-                                        </li>                            
-                                    </ul>
-                                    {/* Teacher */}
-                                    <ul className="flex flex-col gap-2">
-                                        <li>
-                                            <button onClick={() => handleToggleSubMenu('menu1')} 
-                                                        className="w-full flex items-center justify-between cursor-pointer py-2 px-4 rounded-lg hover:bg-teal-400 text-white font-semibold transition-colors">      <span className="flex items-center gap-2"><RiUserLine className="text-secondary-500" />Docentes</span>
-                                                        <RiArrowRightSLine className={`mt-1 ${showSubMenu.menu1 ? 'rotate-90': ''} transition-all`} />
-                                            </button>
-                                            <ul className={`mt-2 ${!showSubMenu.menu1 ? 'hidden' : ''}`}>
-                                                <li>
-                                                    <Link to='registrar-miembro/' className="py-2 px-4 border-l border-slate-400 block ml-6 text-white font-semibold relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary hover:text-gray-400 transition-colors">Registrar docente</Link>
-                                                </li>
-                                                <li>
-                                                    <Link to='miembros' className="py-2 px-4 border-l border-slate-400 block ml-6 text-white font-semibold relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary hover:text-gray-400 transition-colors">Ver docente</Link>
-                                                </li>
-                                            </ul>
-                                        </li>                            
-                                    </ul>                                   
-                                    {/* Message */}
-                                    <ul className="flex flex-col gap-2">
-                                        <li>
-                                            <Link to='/' 
-                                                className="flex items-center gap-3 py-2 px-4 rounded-lg hover:bg-teal-400 text-white font-semibold transition-colors">
-                                                <RiMessage3Fill className="text-secondary-500" />
-                                                Mensajes
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                    {/* Calendar */}
-                                    <ul className="flex flex-col gap-2">
-                                        <li>
-                                            <Link to='/'
-                                                className="flex items-center gap-3 py-2 px-4 rounded-lg hover:bg-teal-400 text-white font-semibold transition-colors">
-                                                <RiCalendarTodoLine className="text-secondary-500" />
-                                                Calendario
-                                            </Link>
-
-                                        </li>
-                                    </ul> 
-                                </ul>
+                                <SidebarStudent
+                                    handleToggleSubMenu={handleToggleSubMenu} 
+                                    showSubmenu={showSubMenu}                                  
+                                />
                             )
                         }                        
                     </nav>
